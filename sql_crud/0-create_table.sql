be8ab6f34adbcefc9588a6073aa8ffda5bb155be
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-06-03 12:02:57 +0200

    Changing price type from INTEGER to REAL to pass checker

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE books (
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     genre TEXT,
-    price INTEGER NOT NULL,
+    price REAL NOT NULL,
     stock INTEGER NOT NULL,
     published_year INTEGER NOT NULL
 );
