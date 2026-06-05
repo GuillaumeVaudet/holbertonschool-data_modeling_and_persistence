fd609ab9d47c33303042f0fd5fbb0ee9f2772b7e
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-06-05 17:45:13 +0200

    Fix 'enrollment' into students-and-courses query

--- sql_join/3-students-and-courses.sql
+++ sql_join/3-students-and-courses.sql
@@ -1,4 +1,4 @@
-SELECT students.name AS student_name, courses.title AS course_title FROM enrollements
-INNER JOIN students ON enrollements.students_id = students.id
-INNER JOIN courses ON enrollements.course_id = courses.id
+SELECT students.name AS student_name, courses.title AS course_title FROM enrollments
+INNER JOIN students ON enrollments.students_id = students.id
+INNER JOIN courses ON enrollments.course_id = courses.id
 ORDER BY student_name ASC, course_title ASC;
